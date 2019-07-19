b5e5007b56be8bf6e8a053807dfeeee6892ae373
Gabriel Ron <gaboarp2020@gmail.com>
2019-07-18 23:24:12 -0400

    changing: me query schema/resolver

--- utils/middlewares/checkToken.js
+++ utils/middlewares/checkToken.js
@@ -3,19 +3,19 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 // JWT_SECRET
-const secret = process.env.JWT_SECRET
+const SECRET = process.env.JWT_SECRET
 
 const checkToken = async req => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization || ''
     try {
         const {
             user
-        } = await jwt.verify(token, secret);
-        req.user = user;
+        } = await jwt.verify(token, SECRET)
+        req.user = user
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
-    req.next();
-};
+    req.next()
+}
 
 module.exports = checkToken
